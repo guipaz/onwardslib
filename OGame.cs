294fec2fb238a9631171712bacb879f86e16e06c
Guilherme Paz Silva <pazsilvag@gmail.com>
2023-06-05 03:14:47 -0300

    .

--- OGame.cs
+++ OGame.cs
@@ -7,7 +7,7 @@ namespace onwardslib
     {
         public OGame()
         {
-            
+            var _ = new GraphicsDeviceManager(this);
         }
 
         protected override void Initialize()
